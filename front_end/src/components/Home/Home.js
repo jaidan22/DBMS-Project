876be3fe27100ec9aebacfb7e6ac4f837e7ec267
Akshay Raj <akrcetcs@gmail.com>
2023-01-02 01:12:15 +0530

    nav & home_bg

---
+++ front_end/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <img src="https://www.metrorailnews.in/wp-content/uploads/2020/07/indian-railways-history-e1663841029626.jpg"/>
+    </div>
+  )
+}
+
+export default Home
---
+++ front_end/src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+        <header>
+           <img src="https://www.clipartmax.com/png/middle/225-2259507_train-logo-train-logo-black-white.png" />
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">PNR Status</a></li>
+                    <li><a href="#">Book Tickets</a></li>
+                    <li><a href="#" className='butt'>Login</a></li>
+                    
+                </ul>
+            </nav>
+           
+        </header>
+    </div>
+  )
+}
+
+export default Navbar
